66cace9bbeef1de8bc1e56bd440ee2b960de7721
Sherwesh Bajracharya <sbajracharya@trioxis.com>
2017-07-01 11:49:14 +1000

    Install Webpack & Babel

--- server/server.js
+++ server/server.js
@@ -26,4 +26,9 @@ app.use('/graphql', expressGraphQL({
   graphiql: true
 }));
 
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
 module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'client/index.html'
+    })
+  ]
+};
