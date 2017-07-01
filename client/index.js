bc5cfa10f7e7cdf89dfa903dd431296dc0cb5a83
Sherwesh Bajracharya <sbajracharya@trioxis.com>
2017-07-01 12:05:02 +1000

    Install & setup React

---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Root = () => {
+  return (
+    <div>
+      to-do-list-app baby
+    </div>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
