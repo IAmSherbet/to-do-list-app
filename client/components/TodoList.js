26d5800f6b9c59254980f3b4db971caaa7e29b31
Sherwesh Bajracharya <sbajracharya@trioxis.com>
2017-08-14 19:13:28 +1000

    Apply material-ui to TodoList

--- client/components/TodoList.js
+++ client/components/TodoList.js
@@ -1,24 +1,57 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Card, CardText, CardActions, CardHeader } from 'material-ui/Card';
+import IconButton from 'material-ui/IconButton';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import DoneIcon from 'material-ui/svg-icons/action/done';
+import DeleteIcon from 'material-ui/svg-icons/action/delete';
+import EditIcon from 'material-ui/svg-icons/editor/mode-edit';
+import s from './todolist.css';
 
 class TodoList extends Component {
   renderSongs() {
     return this.props.data.todos.map(todo => {
-      return (<li key={ todo.id }>{ todo.title }</li>)
+      return (
+        <Card key={ todo.id } className={s.card}>
+          <CardHeader
+            title={ todo.title }
+          />
+          <CardText>{ todo.content }</CardText>
+          <CardActions className={s.cardActions}>
+            <IconButton>
+              <DoneIcon color={'rgba(0,0,0,0.54)'}/>
+            </IconButton>
+            <IconButton>
+              <DeleteIcon color={'rgba(0,0,0,0.54)'}/>
+            </IconButton>
+            <IconButton>
+              <EditIcon color={'rgba(0,0,0,0.54)'}/>
+            </IconButton>
+          </CardActions>
+        </Card>
+      )
     })
   }
 
   render() {
-    if (this.props.data.loading) {
-      return <div>Loading ... </div>
-    } else {
-      return (
-        <ul>
-          { this.renderSongs() }
-        </ul>
-      );
-    }
+    return (
+      <div className={s.todoListWrapper}>
+        { this.props.data.loading
+          ? <div>Loading ... </div>
+          : <div className={s.wrapper}>
+            <div className={s.container}>
+              { this.renderSongs() }
+            </div>
+          </div>
+        }
+        <FloatingActionButton
+          className={s.actionButton}>
+          <ContentAdd />
+        </FloatingActionButton>
+      </div>
+    )
   }
 };
 
@@ -27,6 +60,7 @@ const query = gql`
     todos {
       id
       title
+      content
     }
   }
 `;
