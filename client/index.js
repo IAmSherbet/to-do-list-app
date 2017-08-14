f8f78adb003ac33d3dbef6255047c3b9803e6f7e
Sherwesh Bajracharya <sbajracharya@trioxis.com>
2017-08-14 18:58:51 +1000

    Add material-ui

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { BrowserRouter, Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import TodoList from './components/TodoList';
 
@@ -11,9 +12,13 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <BrowserRouter>
-        <Route exact path="/" component={TodoList}/>
-      </BrowserRouter>
+      <MuiThemeProvider>
+        <div>
+          <BrowserRouter>
+            <Route exact path="/" component={TodoList}/>
+          </BrowserRouter>
+        </div>
+      </MuiThemeProvider>
     </ApolloProvider>
   );
 };
