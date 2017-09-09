714e09dd0accdf1ac1c3d2c69c91e5ad335b4ef3
Sherwesh Bajracharya <sbajracharya@trioxis.com>
2017-09-10 08:38:59 +1000

    Add Drawer: -install react-tap-event-plugin

--- client/index.js
+++ client/index.js
@@ -1,30 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 import TodoList from './components/TodoList';
 import AddTodo from './components/AddTodo';
 
 const client = new ApolloClient({});
 
-const Root = () => {
-  return (
-    <div>
-      <AppBar
-        title="My Todo List App"
-        style={{marginBottom: "24px"}}
-      />
+class Root extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle() {
+    this.setState({ open: !this.state.open });
+  }
+
+  render() {
+    return (
       <div>
-        <Route exact path="/" component={TodoList}/>
-        <Route path="/add" component={AddTodo}/>
+        <AppBar
+          title={<Link to="/">Fortuity Keep</Link>}
+          style={{marginBottom: "24px"}}
+          onLeftIconButtonTouchTap={this.handleToggle}
+        />
+        <Drawer
+          open={this.state.open}
+          docked={false}
+          onRequestChange={this.handleToggle}
+        >
+          <Link
+            to="/"
+            onClick={this.handleToggle}
+          >
+            <MenuItem>Notes</MenuItem>
+          </Link>
+          <Link
+            to="/done"
+            onClick={this.handleToggle}
+          >
+            <MenuItem>Done</MenuItem>
+          </Link>
+        </Drawer>
+        <div>
+          <Route exact path="/" component={TodoList}/>
+          <Route path="/add" component={AddTodo}/>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+injectTapEventPlugin();
 
 ReactDOM.render((
   <ApolloProvider client={client}>
