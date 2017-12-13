c06c7b776e69dedfecd059b41d713f8d730975e5
Sherwesh Bajracharya <sbajracharya@trioxis.com>
2017-12-13 13:41:29 +1100

    Add BrowserSync

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+// test comment
 module.exports = {
   entry: './client/index.js',
   output: {
@@ -33,6 +34,21 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'client/index.html'
-    })
+    }),
+    new BrowserSyncPlugin({
+      // browse to http://localhost:3000/ during development
+      host: 'localhost',
+      port: 4001,
+      // proxy the Webpack Dev Server endpoint
+      // (which should be serving on http://localhost:3000/)
+      // through BrowserSync
+      proxy: 'http://localhost:4000'
+    },
+    {
+      // prevent BrowserSync from reloading the page
+      // and let Webpack Dev Server take care of this
+      reload: false
+    }
+    )
   ]
 };
